cfab4571e3c3bc91807e30ece93f607da55f9c27
Vinicius <viniciusr.camargo@gmail.com>
2025-06-28 21:38:45 -0300

    Tentando terminar o projeto

--- resources/views/categorias/create.blade.php
+++ resources/views/categorias/create.blade.php
@@ -8,7 +8,7 @@
             <div class="mb-3 row">
                 <div class="col-12">
                     <label for="nome" class="form-label">Nome da categoria</label>
-                    <input type="text" class="form-control " id="nome" placeholder="Banco de dados..." name="nome">
+                    <input type="text" class="form-control " id="nome" placeholder="Banco de dados..." name="nome" required>
                 </div>
             </div>
             <button type="submit" class="btn btn-outline-success btn-lg">Adicionar</button>
